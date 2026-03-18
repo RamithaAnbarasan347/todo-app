3da720457be9d91356e0370434f6c841d2eeb08d
RamithaAnbarasan347 <ramitha347@gmail.com>
2026-03-18 10:29:11 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") return;
+
+    let li = document.createElement("li");
+   li.innerHTML = `
+    ${task}
+    <button onclick="completeTask(this)">✔</button>
+    <button onclick="deleteTask(this)">❌</button>
+`;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(btn) {
+    btn.parentElement.remove();
+}
+
+function completeTask(btn) {
+    btn.parentElement.classList.toggle("completed");
+}
